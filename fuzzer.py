b20d5e8469f399c549093292d8c726942a222b89
eyalk7 <eyalk7@gmail.com>
2021-12-17 00:50:37 +0200

    copied packets from recording

--- fuzzer.py
+++ fuzzer.py
@@ -1,96 +1,144 @@
-#This will be our final file
+# This will be our final file
 
 import sys
 from scapy.all import *
 from packets import *
+from binascii import hexlify, unhexlify
 
 PHONE_MAC = "00:00:00:00:00:01"
 DIRECT_SSID = "DIRECT-"
 
-class Fuzzer():
-	def __init__(self, iface, sta_mac=PHONE_MAC, ap_mac=None):
-		self.sta_mac = sta_mac
-		self.ap_mac = ap_mac
-		self.iface = iface
+
+class Fuzzer:
+    def __init__(self, iface, sta_mac=PHONE_MAC, ap_mac=None):
+        self.sta_mac = sta_mac
+        self.ap_mac = ap_mac
+        self.iface = iface
+
+    def _send_req(self, frame, repeats=10):
+        """
+        sends a probe request in a loop until we get a response and returns the response
+        """
+        response = None
+        while not response and repeats > 0:
+            repeats -= 1
+            ans, unans = srp(frame, iface=self.iface, inter=0.2, timeout=0.5)
+            ans.summary()
+            unans.summary()
+            if ans:
+                response = ans[0][1]  # list of answers. each answer is a tuple
+        return response
+
+    def send_probe_req(self, ssid, config_methods, pass_id):
+        frame = create_probe_req(self.sta_mac, ssid, config_methods, pass_id)
+        return self._send_req(frame)
+
+    def send_probe_req_cpy(self, type, ssid, dev_name):
+        frame = create_probe_req_cpy(self.sta_mac, type, ssid, dev_name)
+        return self._send_req(frame)
+
+    def send_prov_disc_req(self, dst_mac):
+        frame = create_prov_disc_req(self.sta_mac, dst_mac)
+        return self._send_req(frame)
+
+    def send_prov_disc_req_cpy(self, dst_mac, dev_name):
+        frame = create_prov_disc_req_cpy(self.sta_mac, dst_mac, dev_name)
+        return self._send_req(frame, repeats=5)
+
+    def send_auth_req(self, dst_mac):
+        frame = create_auth_req(self.sta_mac, dst_mac)
+        return self._send_req(frame)
+
+    def send_auth_req_cpy(self, dst_mac):
+        frame = create_auth_req_cpy(self.sta_mac, dst_mac)
+        return self._send_req(frame, repeats=1)
+
+    def send_asso_req(self, dst_mac, ssid):
+        frame = create_asso_req(self.sta_mac, dst_mac, ssid)
+        return self._send_req(frame)
+
+    def send_asso_req_cpy(self, dst_mac, ssid, dev_name):
+        frame = create_asso_req_cpy(self.sta_mac, dst_mac, ssid, dev_name)
+        return self._send_req(frame, repeats=1)
+
+    def send_null_cpy(self, dst_mac):
+        frame = create_null_cpy(self.sta_mac, dst_mac)
+        return self._send_req(frame)
+
+    def send_block_ack_req(self, dst_mac):
+        frame = create_block_ack_req(self.sta_mac, dst_mac)
+        return self._send_req(frame)
+
+    def send_block_ack_req_cpy(self, dst_mac):
+        frame = create_block_ack_req_cpy(self.sta_mac, dst_mac)
+        return self._send_req(frame, repeats=1)
+
+    def send_eap(self, dst_mac, phase, id=0):
+        frame = create_eap_packet(dst_mac, self.sta_mac, phase, id)
+        return self._send_req(frame)
+
+    def send_eap_start_cpy(self, dst_mac):
+        frame = create_eap_start_cpy(self.sta_mac, dst_mac)
+        return self._send_req(frame)
 
 
-	def _send_req(self, frame, repeats = 500):
-		"""
-		sends a probe request in a loop until we get a response and returns the response
-		"""
-		response = None
-		while not response and repeats > 0:
-			repeats -= 1
-			ans, unans = srp(frame, iface=self.iface, inter=0.5, timeout=0.5)
-			ans.summary()
-			unans.summary()
-			if ans:
-				response = ans[0][1] #list of answers. each answer is a tuple
-		return response
+def regular_protocol(fuzzer):
+    probe_res = fuzzer.send_probe_req(DIRECT_SSID, "0100001111011000", "0000")
 
-	def send_probe_req(self, ssid, config_methods, pass_id):
-		frame = create_probe_req(self.sta_mac, ssid, config_methods, pass_id) 
-		return self._send_req(frame)
+    interface_p2p_addr = probe_res.addr2
+    device_p2p_addr = get_device_p2p_addr(probe_res)
+    sink_ssid = probe_res.info
 
-	def send_prov_disc_req(self, dst_mac):
-		frame = create_prov_disc_req(self.sta_mac, dst_mac)
-		return self._send_req(frame, repeats=1)
+    if device_p2p_addr == None:
+        print("unable to read device P2P address from probe response.")
+        quit()
 
-	def send_auth_req(self, dst_mac):
-		frame = create_auth_req(self.sta_mac, dst_mac) 
-		return self._send_req(frame, repeats=1)
-	
-	def send_asso_req(self, dst_mac, ssid):
-		frame = create_asso_req(self.sta_mac, dst_mac, ssid)
-		return self._send_req(frame, repeats=1)
+    prov_res = fuzzer.send_prov_disc_req(device_p2p_addr)
 
-	def send_block_ack_req(self, dst_mac):
-			frame = create_block_ack_req(self.sta_mac, dst_mac)
-			return self._send_req(frame, repeats=1)
+    while True:
+        probe_res = fuzzer.send_probe_req(sink_ssid, "0011000101001000", "0004")
+        if get_wps_response_type(probe_res) == 2:
+            break
 
-	def send_eap(self, dst_mac, phase, id=0):
-		frame = create_eap_packet(dst_mac, self.sta_mac, phase, id)
-		return self._send_req(frame)
+    auth_res = fuzzer.send_auth_req(interface_p2p_addr)
 
-	
-	
-	
-	
+    asso_res = fuzzer.send_asso_req(interface_p2p_addr, sink_ssid)
+
+    # block_ack_res = fuzzer.send_block_ack_req(interface_p2p_addr)
+
+    eap_start_res = fuzzer.send_eap(interface_p2p_addr, "start")
+
+    # eap_id_res = send_eap(interface_p2p_addr, "id")
+    # after this need to add WPS packages
+
+
+def copied_protocol(
+    fuzzer, interface_p2p_addr, device_p2p_addr, sink_ssid, dev_name="FUZZER"
+):
+    fuzzer.send_probe_req_cpy("first", DIRECT_SSID, dev_name)
+
+    fuzzer.send_prov_disc_req_cpy(device_p2p_addr, dev_name)
+
+    fuzzer.send_probe_req_cpy("second", sink_ssid, dev_name)
+
+    fuzzer.send_auth_req_cpy(interface_p2p_addr)
+
+    fuzzer.send_asso_req_cpy(interface_p2p_addr, sink_ssid, dev_name)
+
+    fuzzer.send_null_cpy(interface_p2p_addr)
+
+    fuzzer.send_block_ack_req_cpy(interface_p2p_addr)
+
+    fuzzer.send_eap_start_cpy(interface_p2p_addr)
+
+
 if __name__ == "__main__":
-	if len(sys.argv) != 2:
-		print("use: sudo python3 fuzzer.py <INTERFACE_NAME>")
-		quit()
-	
-	fuzzer = Fuzzer(sys.argv[1])
+    if len(sys.argv) != 2:
+        print("use: sudo python3 fuzzer.py <INTERFACE_NAME>")
+        quit()
 
-	probe_res = fuzzer.send_probe_req(DIRECT_SSID, "0100001111011000", "0000")
+    fuzzer = Fuzzer(sys.argv[1])
 
-	interface_p2p_addr = probe_res.addr2
-	device_p2p_addr = get_device_p2p_addr(probe_res)
-	sink_ssid = probe_res.info
-
-	if device_p2p_addr == None:
-		print("unable to read device P2P address from probe response.")
-		quit()		
-
-	prov_res = fuzzer.send_prov_disc_req(device_p2p_addr)
-
-	while True:	
-		probe_res = fuzzer.send_probe_req(sink_ssid, "0011000101001000", "0004")
-		if get_wps_response_type(probe_res) == 2:
-			break
-
-	auth_res = fuzzer.send_auth_req(interface_p2p_addr)
-
-	asso_res = fuzzer.send_asso_req(interface_p2p_addr, sink_ssid)
-
-	# block_ack_res = fuzzer.send_block_ack_req(interface_p2p_addr)
-
-	eap_start_res = fuzzer.send_eap(interface_p2p_addr, "start")
-
-	# eap_id_res = send_eap(interface_p2p_addr, "id")
-	# after this need to add WPS packages
-	
-	
-	
-	
+    copied_protocol(
+        fuzzer, "92c5f27aa0a3", "82c5f27aa0a3", "DIRECT-AQLAPTOP-57A31AJJmsML"
+    )
--- packets.py
+++ packets.py
@@ -30,7 +30,7 @@ WPS_ATT = {
     "MODEL_NUM": unhexlify("1024"),
     "DEV_NAME": unhexlify("1011"),
     "VERSION_2": unhexlify("1049"),
-    "RESPONSE_TYPE": unhexlify("103b")
+    "RESPONSE_TYPE": unhexlify("103b"),
 }
 
 
@@ -192,15 +192,15 @@ def wps_version_att():
 
 def wps_req_type_att():
     # 1 bytes hex string
-    # The Request Type component specifies the mode in which the device will operate in for 
-    # this setup exchange.   
+    # The Request Type component specifies the mode in which the device will operate in for
+    # this setup exchange.
     # for now fixed on enrollee, open 802.1x
     return WPS_ATT["REQUEST_TYPE"] + unhexlify("000101")
 
 
 def wps_config_methods_att(config_methods):
-    # 16 bits binary string 
-    # The Configuration Methods Data component lists the configuration methods the 
+    # 16 bits binary string
+    # The Configuration Methods Data component lists the configuration methods the
     # Enrollee or Registrar supports.
     # from right to left - USB, Ethernet, Label, Display, External NFC, Internal NFC
     # NFC Interface, Push Button, Keypad, Virtual Push Button, Physical Push Button
@@ -219,9 +219,7 @@ def wps_uuid_e_att(uuid_e):
     # Enrollee. It uniquely identifies an operational device and should survive reboots and
     # resets.
     bin_uuid_e = unhexlify(uuid_e)
-    return (
-        WPS_ATT["UUID_E"] + len(bin_uuid_e).to_bytes(2, byteorder="big") + bin_uuid_e
-    )
+    return WPS_ATT["UUID_E"] + len(bin_uuid_e).to_bytes(2, byteorder="big") + bin_uuid_e
 
 
 def wps_dev_type_att(dev_type):
@@ -245,19 +243,17 @@ def wps_assoc_state_att():
 
 def wps_conf_error_att():
     # no error
-     return WPS_ATT["CONF_ERROR"] + unhexlify("00020000")
+    return WPS_ATT["CONF_ERROR"] + unhexlify("00020000")
 
 
 def wps_pass_id_att(pass_id):
     bin_pass_id = unhexlify(pass_id)
     return (
-        WPS_ATT["PASS_ID"]
-        + len(bin_pass_id).to_bytes(2, byteorder="big")
-        + bin_pass_id
+        WPS_ATT["PASS_ID"] + len(bin_pass_id).to_bytes(2, byteorder="big") + bin_pass_id
     )
 
 
-def wps_manufacturer_att(manufacturer):   
+def wps_manufacturer_att(manufacturer):
     bin_manufacturer = bytes(manufacturer, encoding="utf8")
     return (
         WPS_ATT["MANUFACTURER"]
@@ -275,7 +271,7 @@ def wps_model_name_att(model_name):
     )
 
 
-def wps_model_num_att(model_num):       
+def wps_model_num_att(model_num):
     bin_model_num = bytes(model_num, encoding="utf8")
     return (
         WPS_ATT["MODEL_NUM"]
@@ -284,7 +280,7 @@ def wps_model_num_att(model_num):
     )
 
 
-def wps_dev_name_att(dev_name):    
+def wps_dev_name_att(dev_name):
     bin_dev_name = bytes(dev_name, encoding="utf8")
     return (
         WPS_ATT["DEV_NAME"]
@@ -308,20 +304,20 @@ def create_probe_req(src_mac, ssid, config_methods, pass_id):
 
     wps_ie_header = wifi_wps_ie_header()
     wps_ie_content = (
-        wps_version_att() +
-        wps_req_type_att() +
-        wps_config_methods_att(config_methods) +
-        wps_uuid_e_att("12d78e1bef9359df8437b8dec4e0c31e") +
-        wps_dev_type_att("000a0050f2040005") +
-        wps_rf_bands_att() + 
-        wps_assoc_state_att() +
-        wps_conf_error_att() +
-        wps_pass_id_att(pass_id) +
-        wps_manufacturer_att("Technion") +
-        wps_model_name_att("CS") +
-        wps_model_num_att("2022") +
-        wps_dev_name_att("FUZZER")+
-        wps_ver_2_att()
+        wps_version_att()
+        + wps_req_type_att()
+        + wps_config_methods_att(config_methods)
+        + wps_uuid_e_att("12d78e1bef9359df8437b8dec4e0c31e")
+        + wps_dev_type_att("000a0050f2040005")
+        + wps_rf_bands_att()
+        + wps_assoc_state_att()
+        + wps_conf_error_att()
+        + wps_pass_id_att(pass_id)
+        + wps_manufacturer_att("Technion")
+        + wps_model_name_att("CS")
+        + wps_model_num_att("2022")
+        + wps_dev_name_att("FUZZER")
+        + wps_ver_2_att()
     )
     wps_ie = wps_ie_header + wps_ie_content
     frame /= Dot11Elt(ID=221, info=RawVal(wps_ie), len=len(wps_ie))
@@ -338,6 +334,50 @@ def create_probe_req(src_mac, ssid, config_methods, pass_id):
     frame /= Dot11Elt(ID=221, info=RawVal(direct_ie), len=len(direct_ie))
 
     return frame
+
+
+def create_probe_req_cpy(src_mac, type, ssid, dev_name):
+    frame = RadioTap()
+    if type == "first":
+        bin_dev_name = bytes(dev_name, encoding="utf8")
+        frame /= Raw(
+            unhexlify(
+                "40000000ffffffffffff" + mac_addr_to_hex(src_mac) + "ffffffffffffe05e"
+            )
+        )
+        frame /= Raw(
+            unhexlify(
+                "00074449524543542d010c02040b0c121618243048606c03010a2d1a210117ffff0000000000000000000000000000000000000000007f080000080000000040dd"
+                + hexlify(
+                    (141 + len(bin_dev_name)).to_bytes(1, byteorder="big")
+                ).decode("utf-8")
+                + "0050f204104a000110103a0001011008000243d81047001012d78e1bef9359df8437b8dec4e0c31f10540008000a0050f2040005103c0001031002000200001009000200001012000200001021001353414d53554e475f454c454354524f4e4943531023000e53414d53554e475f4d4f42494c4510240004323031341011"
+                + hexlify(len(bin_dev_name).to_bytes(2, byteorder="big")).decode(
+                    "utf-8"
+                )
+                + hexlify(bin_dev_name).decode("utf-8")
+                + "1049000900372a000120030101dd0d506f9a0a00000601101c440032dd1300904c0408bf0c3270800ffaff0000faff0000dd070050f208001100dd09001018020000100000dd11506f9a090202002500060500494c045101"
+            )
+        )
+
+    else:
+        bin_ssid = bytes(ssid, encoding="utf8")
+        frame /= Raw(
+            unhexlify(
+                "40000000ffffffffffff" + mac_addr_to_hex(src_mac) + "ffffffffffff705f"
+            )
+        )
+        frame /= Raw(
+            unhexlify(
+                "00"
+                + hexlify(len(bin_ssid).to_bytes(1, byteorder="big")).decode("utf-8")
+                + hexlify(bin_ssid).decode("utf-8")
+                + "01088c129824b048606c03010b2d1a631117ffff0000000000000000000000000000000000000000007f080000080000000040dd6c0050f204104a000110103a0001011008000231481047001012d78e1bef9359df8437b8dec4e0c31f105400080000000000000000103c00010310020002000010090002000010120002000410210001201023000120102400012010110001201049000900372a000120030101dd0d506f9a0a00000601101c440032dd1300904c0408bf0c3270800ffaff0000faff0000dd070050f208001100dd09001018020000100000dd11506f9a090202002500060500494c045101"
+            )
+        )
+
+    return frame
+
 
 def create_prov_disc_req(src_mac, dst_mac):
     # basic headers
@@ -366,12 +406,50 @@ def create_prov_disc_req(src_mac, dst_mac):
     frame /= Dot11Elt(ID=221, info=RawVal(display_ie), len=len(display_ie))
 
     return frame
-        
+
+
+def create_prov_disc_req_cpy(src_mac, dst_mac, dev_name):
+    bin_dev_name = bytes(dev_name, encoding="utf8")
+
+    frame = RadioTap()
+    frame /= Raw(
+        unhexlify("d0003c00" + dst_mac + mac_addr_to_hex(src_mac) + dst_mac + "605f")
+    )
+    frame /= Raw(
+        unhexlify(
+            "0409506f9a090701dd"
+            + hexlify((33 + len(bin_dev_name)).to_bytes(1, byteorder="little")).decode(
+                "utf-8"
+            )
+            + "506f9a0902020025000d"
+            + hexlify((21 + len(bin_dev_name)).to_bytes(2, byteorder="little")).decode(
+                "utf-8"
+            )
+            + mac_addr_to_hex(src_mac)
+            + "0188000a0050f2040005001011"
+            + hexlify(len(bin_dev_name).to_bytes(2, byteorder="big")).decode("utf-8")
+            + hexlify(bin_dev_name).decode("utf-8")
+            + "dd0a0050f204100800020080dd0d506f9a0a00000601101c440032dd0b0000f00a0000040a31a8c0dd470000f01100004024695f2b650d0ebb2bbf5c211c8d7b95100e3cef03642d351964287f2bb8b4e33a336d162519b0a91c5177dc062e003763296667083e2805e6f68eb7aee56411dd090000f00b04ca1c0000"
+        )
+    )
+
+    return frame
+
 
 def create_auth_req(src_mac, dst_mac):
-    frame = RadioTap() 
+    frame = RadioTap()
     frame /= Dot11(addr1=dst_mac, addr2=src_mac, addr3=dst_mac)
     frame /= Dot11Auth(algo=0, seqnum=0x0001, status=0x0000)
+    return frame
+
+
+def create_auth_req_cpy(src_mac, dst_mac):
+    frame = RadioTap()
+    frame /= Raw(
+        unhexlify("b0003c00" + dst_mac + mac_addr_to_hex(src_mac) + dst_mac + "d04f")
+    )
+    frame /= Raw(unhexlify("000001000000dd09001018020000100000"))
+
     return frame
 
 
@@ -384,11 +462,7 @@ def create_asso_req(src_mac, dst_mac, ssid):
     frame /= Dot11Elt(ID=36, info=RawVal(unhexlify("010d")), len=2)
 
     wps_ie_header = wifi_wps_ie_header()
-    wps_ie_content = (
-        wps_version_att() +
-        wps_req_type_att() +
-        wps_ver_2_att()
-    )
+    wps_ie_content = wps_version_att() + wps_req_type_att() + wps_ver_2_att()
     wps_ie = wps_ie_header + wps_ie_content
     frame /= Dot11Elt(ID=221, info=RawVal(wps_ie), len=len(wps_ie))
 
@@ -406,6 +480,47 @@ def create_asso_req(src_mac, dst_mac, ssid):
 
     return frame
 
+
+def create_asso_req_cpy(src_mac, dst_mac, ssid, dev_name):
+    bin_ssid = bytes(ssid, encoding="utf8")
+    bin_dev_name = bytes(dev_name, encoding="utf8")
+
+    frame = RadioTap()
+    frame /= Raw(
+        unhexlify("00003c00" + dst_mac + mac_addr_to_hex(src_mac) + dst_mac + "e04f")
+    )
+    frame /= Raw(
+        unhexlify(
+            "31040a0000"
+            + hexlify(len(bin_ssid).to_bytes(1, byteorder="big")).decode("utf-8")
+            + hexlify(bin_ssid).decode("utf-8")
+            + "01088c129824b048606c210202142402010d2d1aad0117ffff000000000000000000000000000000000000000000dd180050f204104a000110103a0001011049000600372a000120dd0d506f9a0a00000601101c440032dd09001018020000100000dd070050f202000100dd"
+            + hexlify((33 + len(bin_dev_name)).to_bytes(1, byteorder="little")).decode(
+                "utf-8"
+            )
+            + "506f9a0902020027000d"
+            + hexlify((21 + len(bin_dev_name)).to_bytes(2, byteorder="little")).decode(
+                "utf-8"
+            )
+            + mac_addr_to_hex(src_mac)
+            + "0188000a0050f2040005001011"
+            + hexlify(len(bin_dev_name).to_bytes(2, byteorder="big")).decode("utf-8")
+            + hexlify(bin_dev_name).decode("utf-8")
+        )
+    )
+
+    return frame
+
+
+def create_null_cpy(src_mac, dst_mac):
+    frame = RadioTap()
+    frame /= Raw(
+        unhexlify("48012c00" + dst_mac + mac_addr_to_hex(src_mac) + dst_mac + "f04f")
+    )
+
+    return frame
+
+
 def create_block_ack_req(src_mac, dst_mac):
     # basic headers
     frame = RadioTap()
@@ -416,26 +531,54 @@ def create_block_ack_req(src_mac, dst_mac):
 
     return frame
 
+
+def create_block_ack_req_cpy(src_mac, dst_mac):
+    frame = RadioTap()
+    frame /= Raw(
+        unhexlify("d0003c00" + dst_mac + mac_addr_to_hex(src_mac) + dst_mac + "6050")
+    )
+    frame /= Raw(unhexlify("03004d031000000000"))
+
+    return frame
+
+
 def create_eap_packet(dst_mac, src_mac, phase="start", id=0):
-	frame = RadioTap()
-	frame /= Dot11(addr1=dst_mac, addr2=src_mac, addr3=dst_mac, type=0x2, subtype=0x8, FCfield=0x1)
-	frame /= Dot11QoS()
-	frame /= LLC(dsap=0xaa, ssap=0xaa, ctrl=3)
-	frame /= SNAP(OUI=0x0, code=0x888e)
-	
-	if phase == "start":
-		frame /= EAPOL(version=0x1, type=0x1)
-	else:
-		frame /= EAPOL(version=0x1, type=0x0)
-		if phase == "id":
-			eap = EAP(code=0x2, id=id, type=0x1, identity="WFA-SimpleConfig-Enrollee-1-0")
-			eap.len = len(eap)
-			frame /= eap
-		elif phase == "m1":
-			pass
-	
-	return frame 
-	
+    frame = RadioTap()
+    frame /= Dot11(
+        addr1=dst_mac, addr2=src_mac, addr3=dst_mac, type=0x2, subtype=0x8, FCfield=0x1
+    )
+    frame /= Dot11QoS()
+    frame /= LLC(dsap=0xAA, ssap=0xAA, ctrl=3)
+    frame /= SNAP(OUI=0x0, code=0x888E)
+
+    if phase == "start":
+        frame /= EAPOL(version=0x1, type=0x1)
+    else:
+        frame /= EAPOL(version=0x1, type=0x0)
+        if phase == "id":
+            eap = EAP(
+                code=0x2, id=id, type=0x1, identity="WFA-SimpleConfig-Enrollee-1-0"
+            )
+            eap.len = len(eap)
+            frame /= eap
+        elif phase == "m1":
+            pass
+
+    return frame
+
+
+def create_eap_start_cpy(src_mac, dst_mac):
+    frame = RadioTap()
+    frame /= Raw(
+        unhexlify(
+            "88013c00" + dst_mac + mac_addr_to_hex(src_mac) + dst_mac + "00000000"
+        )
+    )
+    frame /= Raw(unhexlify("aaaa03000000888e"))
+    frame /= Raw(unhexlify("01010000"))
+
+    return frame
+
 
 def create_probe_res(dst_mac, src_mac="00:01:02:03:04:05", ssid="DIRECT-TEST"):
     # basic headers
@@ -462,27 +605,33 @@ def create_probe_res(dst_mac, src_mac="00:01:02:03:04:05", ssid="DIRECT-TEST"):
 
 def get_device_p2p_addr(packet):
     ie = packet[Dot11EltVendorSpecific]
-    while isinstance(ie, Dot11EltVendorSpecific):        
-        if ie.info[:3] == WIFI_OUI and ie.info[3] == int.from_bytes(OUI_TYPE["DIRECT"], byteorder='big'):             
+    while isinstance(ie, Dot11EltVendorSpecific):
+        if ie.info[:3] == WIFI_OUI and ie.info[3] == int.from_bytes(
+            OUI_TYPE["DIRECT"], byteorder="big"
+        ):
             i = 4
             while i < len(ie.info):
-                if ie.info[i] == int.from_bytes(DIRECT_ATT["DEVICE_INFO"], byteorder='big'):
-                    return ie.info[i+3:i+9]
-                i += 3 + int.from_bytes(ie.info[i+1:i+3], byteorder='little')
+                if ie.info[i] == int.from_bytes(
+                    DIRECT_ATT["DEVICE_INFO"], byteorder="big"
+                ):
+                    return ie.info[i + 3 : i + 9]
+                i += 3 + int.from_bytes(ie.info[i + 1 : i + 3], byteorder="little")
         ie = ie.payload
 
     return None
-                        
+
 
 def get_wps_response_type(packet):
     ie = packet[Dot11EltVendorSpecific]
     while isinstance(ie, Dot11EltVendorSpecific):
-        if ie.info[:3] == WPS_OUI and ie.info[3] == int.from_bytes(OUI_TYPE["WPS"], byteorder='big'):
+        if ie.info[:3] == WPS_OUI and ie.info[3] == int.from_bytes(
+            OUI_TYPE["WPS"], byteorder="big"
+        ):
             i = 4
             while i < len(ie.info):
-                if ie.info[i:i+2] == WPS_ATT["RESPONSE_TYPE"]:
-                    return ie.info[i+4]
-                i += 4 + int.from_bytes(ie.info[i+2:i+4], byteorder='big')
+                if ie.info[i : i + 2] == WPS_ATT["RESPONSE_TYPE"]:
+                    return ie.info[i + 4]
+                i += 4 + int.from_bytes(ie.info[i + 2 : i + 4], byteorder="big")
         ie = ie.payload
 
-    return None        
+    return None
